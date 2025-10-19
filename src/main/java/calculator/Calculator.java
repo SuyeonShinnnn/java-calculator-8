30704179d85c51126d0cd0140a4a0085d44ddbea
SuyeonShinnnn <suyeons5536@gmail.com>
2025-10-19 22:57:42 +0900

    feat: 숫자 변환 및 입력값 검증 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,20 +3,40 @@ package calculator;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Calculator {
-    private String input;
     private final String basicRegex = ",|:";
 
     public void runCalculator() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        input = readLine();
+        String input = readLine();
 
-        String[] numbers = splitInputByBasicRegex(input);
-        for(String n : numbers){
-            System.out.println(n);
-        }
+        String[] splitInputs = splitInputByBasicRegex(input);
+        int[] numbers = convertStringToInteger(splitInputs);
     }
 
     public String[] splitInputByBasicRegex(String input) {
         return input.split(basicRegex);
+    }
+
+    public int[] convertStringToInteger(String[] inputs) {
+        int[] numbers = new int[inputs.length];
+        for(int i = 0; i < inputs.length; i++) {
+            try{
+                numbers[i] = Integer.parseInt(inputs[i]);
+            } catch (NumberFormatException e) {
+                throw new RuntimeException(e);
+            }
+
+            if(!isProperNumber(numbers[i])) {
+                throw new IllegalArgumentException("잘못된 입력입니다.");
+            }
+        }
+        return numbers;
+    }
+
+    public boolean isProperNumber(int number) {
+        if(number < 0) {
+            return false;
+        }
+        return true;
     }
 }
