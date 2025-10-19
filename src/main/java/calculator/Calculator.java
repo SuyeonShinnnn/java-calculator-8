f2b355ac206df6e6a4fa0883bd169199778a525a
SuyeonShinnnn <suyeons5536@gmail.com>
2025-10-19 22:32:35 +0900

    feat: 기본 구분자 기준으로 입력값 split 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,9 +4,19 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Calculator {
     private String input;
+    private final String basicRegex = ",|:";
 
     public void runCalculator() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         input = readLine();
+
+        String[] numbers = splitInputs(input);
+        for(String n : numbers){
+            System.out.println(n);
+        }
+    }
+
+    public String[] splitInputs(String input) {
+        return input.split(basicRegex);
     }
 }
